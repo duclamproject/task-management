e6e12000feb9a267ffe276960bd03c40e1a82ab1
Duc <ngohoaiduc08@gmail.com>
2025-04-21 06:40:23 +0700

    Done: Loc theo trang thai

---
+++ api/v1/controllers/task.controller.js
@@ -0,0 +1,47 @@
+const Task = require("../models/task.model");
+
+// [Get] /api/v1/task/
+module.exports.index = async (req, res) => {
+  const find = {
+    deleted: false,
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  // Pagination
+  // Công thức phân trang (Skip)= (CurrentPage - 1) * LimtIems;
+  //   let initPagination = {
+  //     currentPage: 1,
+  //     limitItems: 4,
+  //   };
+  //   const countTasks = await Task.countDocuments(find);
+  //   const objectPagination = paginationHelper(
+  //     initPagination,
+  //     req.query,
+  //     countTasks
+  //   );
+  // End Pagination
+
+  // Sort
+  const sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  } else {
+    sort.title = "desc";
+  }
+  // End Sort
+  const tasks = await Task.find(find).sort(sort);
+  res.json(tasks);
+};
+
+// [GET] /api/v1/task/detail/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findOne({ _id: id, deleted: false });
+    res.json(task);
+  } catch (error) {
+    res.json("Không tìm thấy");
+  }
+};
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,25 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/task.controller");
 
-const Task = require("../../../models/task.model");
+router.get("/", controller.index);
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find({ deleted: false });
-  console.log(tasks);
-
-  res.json(tasks);
-});
-
-router.get("/detail/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const task = await Task.findOne({ _id: id, deleted: false });
-    console.log(task);
-
-    res.json(task);
-  } catch (error) {
-    res.json("Không tìm thấy");
-  }
-});
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
