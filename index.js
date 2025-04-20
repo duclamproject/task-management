c7519734e5c97dce352639374d93ee91d8630c4c
Duc <ngohoaiduc08@gmail.com>
2025-04-21 05:38:43 +0700

    Done: Tao routes

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require("express");
 const database = require("./config/database");
-const env = require("dotenv").config();
+require("dotenv").config();
+
+const routerApiVer1 = require("./api/v1/routes/index.route");
+
 const app = express();
 const port = process.env.PORT;
 
 database.connect();
 
-const Task = require("./models/task.model");
-
-app.get("/task", async (req, res) => {
-  const tasks = await Task.find({ deleted: false });
-  console.log(tasks);
-
-  res.json(tasks);
-});
-
-app.get("/task/detail/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const task = await Task.findOne({ _id: id, deleted: false });
-    console.log(task);
-
-    res.json(task);
-  } catch (error) {
-    res.json("Không tìm thấy");
-  }
-});
+// ROUTER API VER 1
+routerApiVer1(app);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
