a5f643927a689c0b54faba616c81899172b4ae3f
Duc <ngohoaiduc08@gmail.com>
2025-04-24 21:23:26 +0700

    Done: cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const database = require("./config/database");
 const bodyParser = require("body-parser");
+var cors = require("cors");
 require("dotenv").config();
 
 const routerApiVer1 = require("./api/v1/routes/index.route");
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 database.connect();
 
