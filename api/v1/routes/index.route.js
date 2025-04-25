c8332fd1df415b2d136c2fea4fe788153b8cb63d
Duc <ngohoaiduc08@gmail.com>
2025-04-25 12:26:36 +0700

    Done: register

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -1,7 +1,7 @@
-const taskRoute = require("./task.route");
-const userRoute = require("./user.route");
+const taskRoutes = require("./task.route");
+const userRoutes = require("./user.route");
 module.exports = (app) => {
   const version = "/api/v1";
-  app.use(version + "/task", taskRoute);
-  app.use(version + "/user", userRoute);
+  app.use(version + "/tasks", taskRoutes);
+  app.use(version + "/users", userRoutes);
 };
