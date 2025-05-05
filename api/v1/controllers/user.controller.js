0bd4ed28375f68607a2967b14e182f5f87fb356e
Duc <ngohoaiduc08@gmail.com>
2025-05-05 19:10:58 +0700

    Done: User detail

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -159,3 +159,18 @@ module.exports.resetPassword = async (req, res) => {
     message: "Đặt lại mật khẩu thành công!",
   });
 };
+
+// [GET] /user/detail
+module.exports.detail = async (req, res) => {
+  const token = req.cookies.token;
+  const user = await User.findOne({
+    token: token,
+    deleted: false,
+  }).select("-password -token");
+
+  res.json({
+    code: 200,
+    message: "Thành công!",
+    info: user,
+  });
+};
