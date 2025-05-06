9e0050f51c1725dda567b1944b62e24ae52580b8
Duc <ngohoaiduc08@gmail.com>
2025-05-06 17:47:27 +0700

    Done: Task children

--- api/v1/models/task.model.js
+++ api/v1/models/task.model.js
@@ -8,6 +8,7 @@ const taskShema = new mongoose.Schema(
     timeFinish: Date,
     createdBy: String,
     listUser: Array,
+    taskParentId: String,
     deleted: {
       type: Boolean,
       default: false,
